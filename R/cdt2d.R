76639e98df4f43b183dd724eddb4640780116f9d
Michael Sumner <mdsumner@gmail.com>
2018-03-28 23:51:27 +0000

    raw function for points-only

--- R/cdt2d.R
+++ R/cdt2d.R
@@ -6,3 +6,26 @@ load_cdt2d <- function() {
   ct
 }
 
+## numbers to json
+toj <- function(x) {
+  sprintf("[%s]", paste(x, collapse = ", "))
+}
+## x,y numbers to json
+tojxy <- function(x, y) {
+  sprintf("[%s]", paste(paste(x, y, sep = ","), collapse = ", "))
+}
+## x,y numbers array
+tojaxy <- function(x, y) {
+  sprintf("[%s]", paste(sprintf("[%f,%f]", x, y), collapse = ","))
+}
+get_tri <- function(x, y, edges = NULL) {
+  stopifnot(length(x) == length(y))
+  stopifnot(!(any(is.na(x)) | any(is.na(y))))
+  ct <- load_cdt2d()
+  if (is.null(edges)) {
+    ct$eval(sprintf("var triangles = cdt2d(%s)", tojaxy(x, y)))
+  } else {
+
+  }
+  ct$get("triangles") + 1L
+}
